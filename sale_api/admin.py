d67551dccd54c6d1d052c76e3024b7ff0ac1897b
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-02-23 19:29:56 +0600

    done: issue#18 - sale

--- sale_api/admin.py
+++ sale_api/admin.py
@@ -1,3 +1,17 @@
+# sale_api/admin.py
 from django.contrib import admin
+from .models import Sale, SaleItem
 
-# Register your models here.
+
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'sale_date',
+                    'invoice_number', 'status', 'total_amount', 'company_id']
+    list_filter = ['status', 'sale_date']
+    search_fields = ['invoice_number']
+
+
+@admin.register(SaleItem)
+class SaleItemAdmin(admin.ModelAdmin):
+    list_display = ['sale', 'product_variant',
+                    'quantity', 'unit_price', 'line_total']
