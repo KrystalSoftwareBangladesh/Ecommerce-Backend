7c2f80e216bd1b2eb2a0e333457346026cdb05db
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-03-17 14:06:42 +0600

    done: issue#53 - opening transaction with account create

--- account_api/serializers/__init__.py
+++ account_api/serializers/__init__.py
@@ -2,10 +2,12 @@ from .chart_of_account import (
     ChartOfAccountCreateUpdateSerializer,
     ChartOfAccountDetailSerializer,
     ChartOfAccountListSerializer,
+    ChartOfAccountOpeningBalanceSerializer,
 )
 
 __all__ = [
     'ChartOfAccountCreateUpdateSerializer',
     'ChartOfAccountDetailSerializer',
     'ChartOfAccountListSerializer',
+    'ChartOfAccountOpeningBalanceSerializer',
 ]
--- account_api/views/v1/chart_of_account.py
+++ account_api/views/v1/chart_of_account.py
@@ -1,8 +1,10 @@
+from django.db import transaction
 from django.utils import timezone
 from django_filters.rest_framework import DjangoFilterBackend
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
 from rest_framework import filters, status, viewsets
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
@@ -12,9 +14,19 @@ from account_api.serializers import (
     ChartOfAccountCreateUpdateSerializer,
     ChartOfAccountDetailSerializer,
     ChartOfAccountListSerializer,
+    ChartOfAccountOpeningBalanceSerializer,
 )
+from account_api.services import set_opening_balance
 
 
+@extend_schema_view(
+    create=extend_schema(
+        responses={201: ChartOfAccountDetailSerializer}
+    ),
+    partial_update=extend_schema(
+        responses={200: ChartOfAccountDetailSerializer}
+    ),
+)
 @extend_schema(tags=["Accounts"])
 class ChartOfAccountViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
@@ -33,6 +45,7 @@ class ChartOfAccountViewSet(viewsets.ModelViewSet):
             deleted_at__isnull=True
         ).select_related(
             'parent',
+            'opening_transaction',
             'created_by',
             'updated_by',
         )
@@ -42,7 +55,51 @@ class ChartOfAccountViewSet(viewsets.ModelViewSet):
             return ChartOfAccountListSerializer
         if self.action == 'retrieve':
             return ChartOfAccountDetailSerializer
+        if self.action == 'set_opening_balance':
+            return ChartOfAccountOpeningBalanceSerializer
         return ChartOfAccountCreateUpdateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        opening_balance = serializer.validated_data.pop(
+            'opening_balance',
+            None,
+        )
+        opening_date = serializer.validated_data.pop('opening_date', None)
+        contra_account = serializer.validated_data.pop(
+            'opening_contra_account',
+            None,
+        )
+
+        try:
+            with transaction.atomic():
+                account = serializer.save(
+                    created_by=request.user,
+                    updated_by=request.user,
+                )
+                if opening_balance is not None:
+                    account = set_opening_balance(
+                        account=account,
+                        amount=opening_balance,
+                        date=opening_date,
+                        user=request.user,
+                        contra_account=contra_account,
+                    )
+        except Exception as exc:
+            return Response(
+                {'detail': str(exc)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(
+            ChartOfAccountDetailSerializer(
+                account,
+                context=self.get_serializer_context(),
+            ).data,
+            status=status.HTTP_201_CREATED,
+        )
 
     def perform_create(self, serializer):
         serializer.save(
@@ -52,6 +109,22 @@ class ChartOfAccountViewSet(viewsets.ModelViewSet):
 
     def perform_update(self, serializer):
         serializer.save(updated_by=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance,
+            data=request.data,
+            partial=True,
+        )
+        serializer.is_valid(raise_exception=True)
+        account = serializer.save(updated_by=request.user)
+        return Response(
+            ChartOfAccountDetailSerializer(
+                account,
+                context=self.get_serializer_context(),
+            ).data
+        )
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -65,3 +138,31 @@ class ChartOfAccountViewSet(viewsets.ModelViewSet):
             'updated_at',
         ])
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=['post'], url_path='set-opening-balance')
+    def set_opening_balance(self, request, pk=None):
+        account = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        try:
+            account = set_opening_balance(
+                account=account,
+                amount=serializer.validated_data['amount'],
+                date=serializer.validated_data['date'],
+                user=request.user,
+                contra_account=serializer.validated_data.get('contra_account'),
+            )
+        except Exception as exc:
+            return Response(
+                {'detail': str(exc)},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        return Response(
+            ChartOfAccountDetailSerializer(
+                account,
+                context=self.get_serializer_context(),
+            ).data,
+            status=status.HTTP_200_OK,
+        )
