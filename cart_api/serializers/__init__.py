908bafad937aa74e9bc266a2d404e22534838a0a
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-08-01 02:55:02 +0600

    done: issue#182 - cart serializers

--- cart_api/serializers/__init__.py
+++ cart_api/serializers/__init__.py
@@ -1,30 +1,14 @@
 # cart_api/serializers/__init__.py
 from .cart import (
-    CartCreateSerializer,
-    CartDetailSerializer,
-    CartListSerializer,
-    CartUpdateSerializer,
+    CartSerializer,
 )
 from .cart_item import (
-    CartItemCreateSerializer,
-    CartItemListSerializer,
-    CartItemDetailSerializer,
-    CartItemUpdateSerializer,
-)
-from .cart_type import (
-    CartTypeCreateUpdateSerializer,
-    CartTypeSerializer,
+    CartItemSerializer,
+    CartItemCreateUpdateSerializer,
 )
 
 __all__ = [
-    "CartCreateSerializer",
-    "CartDetailSerializer",
-    "CartListSerializer",
-    "CartUpdateSerializer",
-    "CartItemCreateSerializer",
-    "CartItemDetailSerializer",
-    "CartItemListSerializer",
-    "CartItemUpdateSerializer",
-    "CartTypeCreateUpdateSerializer",
-    "CartTypeSerializer",
+    "CartSerializer",
+    "CartItemSerializer",
+    "CartItemCreateUpdateSerializer",
 ]
