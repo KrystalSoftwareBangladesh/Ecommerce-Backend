fdada1561fac5c34a48c15fbf308ed102d155e22
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-08-09 01:01:19 +0600

    fixed: issue#209 - prevent self user deletion

--- user_api/views/v1/user.py
+++ user_api/views/v1/user.py
@@ -1,6 +1,7 @@
 # user_api/views/v1/user.py
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from rest_framework import generics, permissions
 from rest_framework import status
 from rest_framework import viewsets
@@ -48,6 +49,23 @@ class UserViewSet(viewsets.ModelViewSet):
         permissions.IsAuthenticated,
         permissions.IsAdminUser,
     ]
+
+    def destroy(self, request, *args, **kwargs):
+        user = self.get_object()
+        if user.pk == request.user.pk:
+            raise PermissionDenied(
+                "You cannot delete your own user account."
+            )
+
+        user.delete()
+
+        return Response(
+            {
+                "success": True,
+                "message": "User deleted successfully.",
+            },
+            status=status.HTTP_200_OK,
+        )
 
 
 class AssignGroupView(generics.UpdateAPIView):
