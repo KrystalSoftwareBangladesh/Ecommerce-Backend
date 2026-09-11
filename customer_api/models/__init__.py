7d1936f7da90075bb09182a242fb2b0f5059c570
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-09-12 01:22:54 +0600

    done: issue#351 - customer address

--- customer_api/models/__init__.py
+++ customer_api/models/__init__.py
@@ -1,7 +1,9 @@
 # customer_api/models/__init__.py
 from .customer import CustomerProfile
+from .customer_address import CustomerAddress
 
 
 __all__ = [
     "CustomerProfile",
+    "CustomerAddress",
 ]
