d7cea3cb1cacdf13483d2a76e2357d81af37ffeb
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-09-11 21:33:05 +0600

    done: issue#351 - customer model re-organized

---
+++ customer_api/models/__init__.py
@@ -0,0 +1,7 @@
+# customer_api/models/__init__.py
+from .customer import CustomerProfile
+
+
+__all__ = [
+    "CustomerProfile",
+]
--- customer_api/models/customer.py
+++ customer_api/models/customer.py
@@ -1,4 +1,4 @@
-
+# customer_api/models/customer.py
 from django.db import models
 from django.contrib.auth import get_user_model
 
