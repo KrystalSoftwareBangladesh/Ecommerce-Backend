78e88e680c5162f07d61fc3c237ebdfbe47a1673
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-09-18 01:27:27 +0600

    done: issue#383 - block delete category if it has sub-categories, blog posts, products

--- category_api/services/__init__.py
+++ category_api/services/__init__.py
@@ -1,11 +1,13 @@
 # category_api/services/__init__.py
 from .category import CategoryImportService
-from .category import get_category_descendant_ids
-from .category import get_category_price_range
+from .category import (
+    get_category_descendant_ids, get_category_price_range, delete_category,
+)
 
 
 __all__ = [
     "CategoryImportService",
     "get_category_descendant_ids",
     "get_category_price_range",
+    "delete_category",
 ]
