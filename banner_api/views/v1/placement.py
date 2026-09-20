7652e95c89eb4cb702505d310f12c0d30d6e90cd
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-09-21 01:11:41 +0600

    fixed: issue#404 - placement API made public

--- banner_api/views/v1/placement.py
+++ banner_api/views/v1/placement.py
@@ -2,12 +2,16 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
 
+
+from EcommerceBackend.core.permission import (
+    PublicReadPermissionMixin,
+)
 from banner_api.filters import BannerPlacementFilter
 from banner_api.models import BannerPlacement
 from banner_api.serializers import BannerPlacementSerializer
 
 
-class BannerPlacementViewSet(viewsets.ModelViewSet):
+class BannerPlacementViewSet(PublicReadPermissionMixin, viewsets.ModelViewSet):
     queryset = BannerPlacement.objects.all()
     serializer_class = BannerPlacementSerializer
 
